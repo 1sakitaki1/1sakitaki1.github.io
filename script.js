44dd3596d69b5136e0a97c28b20ec90b98f1060d
1sakitaki1 <110219849+1sakitaki1@users.noreply.github.com>
2023-06-22 16:59:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,120 +2,129 @@ document.addEventListener("DOMContentLoaded", function() {
 	var letterElement = document.getElementById("letter");
 	var questionElement = document.getElementById("question");
 	var generateButton = document.getElementById("generate-btn");
-  
-	generateButton.addEventListener("click", function() {
-	  var randomLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
-	  letterElement.textContent = randomLetter;
-	  questionElement.style.opacity = 0;
-	  setTimeout(function() {
-		generateRandomQuestion();
-		questionElement.style.opacity = 1;
-	  }, 1000);
-	});
-  });
-  
-  // Get the letter and question elements
-  const letterElement = document.getElementById("letter");
-  const questionElement = document.getElementById("question");
-  
-  // Function to generate a random letter
-  function generateLetter() {
-	const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	const randomIndex = Math.floor(Math.random() * letters.length);
-	const randomLetter = letters.charAt(randomIndex);
-	letterElement.textContent = randomLetter;
-  }
-  
-  // Function to generate a random question
-  function generateRandomQuestion() {
-	const randomLetter = letterElement.textContent;
-	const questions = [
-	  "Name a person that starts with the letter " + randomLetter,
-	  "Name an animal that starts with the letter " + randomLetter,
-	  "Name a country that starts with the letter " + randomLetter,
-	  "Name a food that starts with the letter " + randomLetter,
-	];
-	const randomIndex = Math.floor(Math.random() * questions.length);
-	const randomQuestion = questions[randomIndex];
-	questionElement.textContent = randomQuestion;
-  }
-  
-  // Function to reset the letter and question
-  function resetLetter() {
-	letterElement.textContent = "";
-	questionElement.textContent = "";
-  }
-  
- 
-  
+	var stopTimerButton = document.getElementById("stop-timer-btn");
+	var continueTimerButton = document.getElementById("continue-timer-btn");
+	var resetButton = document.getElementById("reset-btn");
+	var timerElement = document.getElementById("timer");
+	var timerInterval;
+	var initialTime = 10; // Initial timer duration
+	var remainingTime = initialTime; // Track the remaining time
 
-  document.addEventListener("DOMContentLoaded", function() {
-	var letterElement = document.getElementById("letter");
-	var questionElement = document.getElementById("question");
-  
-	function generateRandomQuestion() {
-	  var randomLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
-	  letterElement.textContent = randomLetter;
-	  questionElement.style.opacity = 0;
-	  setTimeout(function() {
-		questionElement.textContent =
-		  "Name a person that starts with the letter " + randomLetter;
-		questionElement.style.opacity = 1;
-	  }, 1000);
-	}
-  
-	function resetLetter() {
-	  letterElement.textContent = "";
-	  questionElement.textContent = "";
-	}
-  
-	generateRandomQuestion();
-  });
-  
-  document.addEventListener("DOMContentLoaded", function() {
-	var letterElement = document.getElementById("letter");
-	var questionElement = document.getElementById("question");
-	var previousQuestion = ""; // Keep track of the previous question
-  
-	function generateRandomQuestion() {
-	  var randomLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
-	  letterElement.textContent = randomLetter;
-	  questionElement.style.opacity = 0;
+	generateButton.addEventListener("click", function() {
+		startTimer();
+		generateRandomLetter();
+		generateRandomQuestion();
+	  })
 	  
-	  setTimeout(function() {
-		var randomQuestion = generateUniqueQuestion(randomLetter);
-		questionElement.textContent = randomQuestion;
-		questionElement.style.opacity = 1;
-	  }, 1000);
+	  resetButton.addEventListener("click", function() {
+		resetTimer();
+	  });
+
+	  stopTimerButton.addEventListener("click", function() {
+		stopTimer();
+	  });
+
+	  continueTimerButton.addEventListener("click", function() {
+		continueTimer();
+	  });
+
+	generateButton.addEventListener("click", function() {
+		var randomLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+		letterElement.textContent = randomLetter;
+		letterElement.style.animation = "fade-in 1s";
+		questionElement.style.opacity = 0;
+	
+		generateButton.disabled = true; // Disable the button during the animation
+	
+		setTimeout(function() {
+		  generateRandomQuestion();
+		  questionElement.style.opacity = 1;
+		  generateButton.disabled = false; // Re-enable the button after the animation
+		}, 1000);
+	  });
+  
+	resetButton.addEventListener("click", function() {
+	  resetLetter();
+	});
+  
+	// Function to generate a random letter
+	function generateRandomLetter() {
+	  var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	  var randomIndex = Math.floor(Math.random() * letters.length);
+	  var randomLetter = letters.charAt(randomIndex);
+	  letterElement.textContent = randomLetter;
 	}
   
-	// Function to generate a unique question different from the previous one
-	function generateUniqueQuestion(randomLetter) {
+	// Function to generate a random question
+	function generateRandomQuestion() {
+	  var randomLetter = letterElement.textContent;
 	  var questions = [
-		"Name a person that starts with the letter " + randomLetter,
+		"Name a person whose name starts with the letter " + randomLetter,
 		"Name an animal that starts with the letter " + randomLetter,
 		"Name a country that starts with the letter " + randomLetter,
 		"Name a food that starts with the letter " + randomLetter,
+		"Name a sport that starts with the letter " + randomLetter,
+		"Name a car brand that starts with the letter " + randomLetter,
+		"Name a movie that starts with the letter " + randomLetter,
 	  ];
-  
-	  var randomIndex;
-	  var randomQuestion;
-  
-	  do {
-		randomIndex = Math.floor(Math.random() * questions.length);
-		randomQuestion = questions[randomIndex];
-	  } while (randomQuestion === previousQuestion); // Repeat until a different question is generated
-  
-	  previousQuestion = randomQuestion; // Update the previous question
-	  return randomQuestion;
+	  var randomIndex = Math.floor(Math.random() * questions.length);
+	  var randomQuestion = questions[randomIndex];
+	  questionElement.textContent = randomQuestion;
 	}
   
+  // Function to start the timer
+  function startTimer() {
+    resetTimer();
+    var time = 10;
+    timerElement.textContent = time;
+    timerInterval = setInterval(function() {
+      time--;
+      timerElement.textContent = time;
+      if (time <= 0) {
+        clearInterval(timerInterval);
+        startTimer();
+        generateRandomLetter();
+        generateRandomQuestion(); // Added to generate a new random question
+      }
+    }, 1000);
+  }
+
+  function stopTimer() {
+    clearInterval(timerInterval);
+  }
+
+  function continueTimer() {
+    startTimer();
+  }
+
+  // Function to reset the timer
+  function resetTimer() {
+    clearInterval(timerInterval);
+    timerElement.textContent = 10;
+  }
+
+  function resetTimer() {
+    clearInterval(timerInterval);
+    remainingTime = initialTime; // Reset the remaining time to initial time
+    timerElement.textContent = remainingTime;
+  }
+
+  // Initial setup
+  generateRandomLetter(); // Added to display an initial random letter
+  generateRandomQuestion(); // Added to display an initial random question
+  resetTimer();
+
+
+	// Function to reset the letter and question
 	function resetLetter() {
+	  letterElement.style.animation = "";
 	  letterElement.textContent = "";
 	  questionElement.textContent = "";
 	}
   
+	// Initial setup
 	generateRandomQuestion();
   });
   
+
   
